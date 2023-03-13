1a06da063cbbe5d6836938f8af0b2c5b6899eb99
Arthur <valentin.arthur1000@gmail.com>
2023-03-13 16:01:36 +0100

    Create user

---
+++ symfony_api/src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+}
