9d27071eafe5f6846d3e3a55a91fdaf91a01f232
Swartline <77667035+Swartline@users.noreply.github.com>
2023-03-13 17:03:24 +0100

    install ezAdmin

--- symfony_api/src/Entity/User.php
+++ symfony_api/src/Entity/User.php
@@ -10,7 +10,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ApiResource()]
+#[ApiResource(
+)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
