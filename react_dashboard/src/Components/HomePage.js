c385be086f77ed56f09ce83858e479d1c796622c
Arthur <valentin.arthur1000@gmail.com>
2023-04-18 10:59:21 +0200

    Fix links

--- react_dashboard/src/Components/HomePage.js
+++ react_dashboard/src/Components/HomePage.js
@@ -20,7 +20,7 @@ function HomePage() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
                 <div className="container">
-                    <a className="navbar-brand" href="#">ConcoursPhoto.com</a>
+                    <a className="navbar-brand" href="/">ConcoursPhoto.com</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
@@ -29,7 +29,7 @@ function HomePage() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item nav-link-bold">
-                                <a className="nav-link active" aria-current="page" href="#">Accueil</a>
+                                <a className="nav-link active" aria-current="page" href="/">Accueil</a>
                             </li>
                             <li className="nav-item nav-link-bold">
                                 <a className="nav-link" href="#">Concours photo</a>
--- react_dashboard/src/Components/InfoContest.js
+++ react_dashboard/src/Components/InfoContest.js
@@ -40,7 +40,7 @@ function InfoContest() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
                 <div className="container">
-                    <a className="navbar-brand" href="#">ConcoursPhoto.com</a>
+                    <a className="navbar-brand" href="/">ConcoursPhoto.com</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
@@ -49,7 +49,7 @@ function InfoContest() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item nav-link-bold">
-                                <a className="nav-link active" aria-current="page" href="#">Accueil</a>
+                                <a className="nav-link active" aria-current="page" href="/">Accueil</a>
                             </li>
                             <li className="nav-item nav-link-bold">
                                 <a className="nav-link" href="#">Concours photo</a>
